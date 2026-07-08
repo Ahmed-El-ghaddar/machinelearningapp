faa88652484224347c914b3ecb2f0cc408309f04
Ahmed El ghaddar <ahmedghaddar0011@gmail.com>
2026-07-08 13:12:47 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 st.title('🤖machine learning')
 st.info("this is a machine learning application for test")
-with st.expander("Data")
+with st.expander("Data"):
   st.write('** Row Data')
   df=pd.read_csv("penguins_cleaned.csv")
   df
