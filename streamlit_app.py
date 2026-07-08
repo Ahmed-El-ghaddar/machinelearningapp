2db65e75c187930ffd28cc739258d56646064dc6
Ahmed El ghaddar <ahmedghaddar0011@gmail.com>
2026-07-08 13:18:36 +0300

    Fix variable assignment for x by using df

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ with st.expander("Data"):
   df=pd.read_csv("penguins_cleaned.csv")
   df
   st.write('x')
-  x=pd.drop('Species'.axis=1)
+  x=df.drop('Species'.axis=1)
   x
   st.write('Y')
   y=df.Species
