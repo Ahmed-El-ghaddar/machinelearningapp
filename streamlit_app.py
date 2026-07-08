c462563b24a75198522755a8e8126db9ea42e500
Ahmed El ghaddar <ahmedghaddar0011@gmail.com>
2026-07-08 13:57:25 +0300

    Fix indentation in expander context manager

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ with st.sidebar:
   input_df = pd.DataFrame(data, index=[0])
   input_penguins = pd.concat([input_df, x], axis=0)
 
-with expander("Input Features")
+with expander("Input Features"):
   st.write("***Iput Data***")
   input_df
   st.write("**Combined penguins data***")
