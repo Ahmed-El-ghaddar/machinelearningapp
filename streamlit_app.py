5e27e08373616f7ef6da177e97370f1f0540ca71
Ahmed El ghaddar <ahmedghaddar0011@gmail.com>
2026-07-08 13:20:24 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,10 +6,10 @@ with st.expander("Data"):
   st.write('** Row Data')
   df=pd.read_csv("penguins_cleaned.csv")
   df
-  st.write('x')
+  st.write('***x***')
   x=df.drop('species',axis=1)
   x
-  st.write('Y')
+  st.write('***Y***')
   y=df.species
   y
 
